ab9eb72eae40729e8b1f0e7c375bf052381bac8c
Felix <felix@t495.fritz.box>
2020-11-11 18:09:17 +0100

    use no check certificate to download data

--- part_seg/download_data.sh
+++ part_seg/download_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Download original ShapeNetPart dataset (around 1GB)
-wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_v0.zip
+wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_v0.zip  --no-check-certificate
 unzip shapenetcore_partanno_v0.zip
 rm shapenetcore_partanno_v0.zip
 
 # Download HDF5 for ShapeNet Part segmentation (around 346MB)
-wget https://shapenet.cs.stanford.edu/media/shapenet_part_seg_hdf5_data.zip
+wget https://shapenet.cs.stanford.edu/media/shapenet_part_seg_hdf5_data.zip --no-check-certificate
 unzip shapenet_part_seg_hdf5_data.zip
 rm shapenet_part_seg_hdf5_data.zip
+
 
