f9850d3cd6b0ea75babdb51b5e078bc18fc96d87
Felix <felix@t495.fritz.box>
2020-11-18 18:06:16 +0100

    use no check certificate to download sem_seg data

--- sem_seg/download_data.sh
+++ sem_seg/download_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download HDF5 for indoor 3d semantic segmentation (around 1.6GB)
-wget https://shapenet.cs.stanford.edu/media/indoor3d_sem_seg_hdf5_data.zip
+wget https://shapenet.cs.stanford.edu/media/indoor3d_sem_seg_hdf5_data.zip --no-check-certificate
 unzip indoor3d_sem_seg_hdf5_data.zip
 rm indoor3d_sem_seg_hdf5_data.zip
 
