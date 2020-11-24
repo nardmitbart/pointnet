cc40da2ce5bf659b9da526186e58cab17a38edc0
Felix <felix@t495.fritz.box>
2020-11-24 21:48:02 +0100

    export training prediction to csv file

--- sem_seg/train.py
+++ sem_seg/train.py
@@ -15,12 +15,18 @@ sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 import provider
 import tf_util
 from model import *
+import csv
+ 
+# see: https://stackoverflow.com/questions/11125878/python-output-to-file-higher-floating-point-precision
+class myFloat( float ):
+    def __str__(self):
+        return "%.6f"%self
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--log_dir', default='log6', help='Log dir [default: log]')
 parser.add_argument('--num_point', type=int, default=2048, help='Point number [default: 4096]')
-parser.add_argument('--max_epoch', type=int, default=50, help='Epoch to run [default: 50]')
+parser.add_argument('--max_epoch', type=int, default=51, help='Epoch to run [default: 50]')
 parser.add_argument('--batch_size', type=int, default=1, help='Batch Size during training [default: 24]')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
 parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
@@ -174,7 +180,7 @@ def train():
             log_string('**** EPOCH %03d ****' % (epoch))
             sys.stdout.flush()
              
-            train_one_epoch(sess, ops, train_writer)
+            train_one_epoch(sess, ops, train_writer, epoch)
             eval_one_epoch(sess, ops, test_writer)
             
             # Save the variables to disk.
@@ -184,7 +190,7 @@ def train():
 
 
 
-def train_one_epoch(sess, ops, train_writer):
+def train_one_epoch(sess, ops, train_writer, epoch):
     """ ops: dict mapping from string to tf ops """
     is_training = True
     
@@ -197,6 +203,8 @@ def train_one_epoch(sess, ops, train_writer):
     total_correct = 0
     total_seen = 0
     loss_sum = 0
+
+    data = [] # for debug exporting results
     
     for batch_idx in range(num_batches):
         if batch_idx % 100 == 0:
@@ -209,12 +217,38 @@ def train_one_epoch(sess, ops, train_writer):
                      ops['is_training_pl']: is_training,}
         summary, step, _, loss_val, pred_val = sess.run([ops['merged'], ops['step'], ops['train_op'], ops['loss'], ops['pred']],
                                          feed_dict=feed_dict)
+
+        # coords
+        #print(current_data[start_idx:end_idx, :, :]) # xyz
+        #print(current_data[start_idx:end_idx, :, :].shape) # (1, 2048, 3) 
+        
+        #print(pred_val)
+        #print(pred_val.shape) # (1, 2048, NUM_CLASSES in model.py/get_model())
+
         train_writer.add_summary(summary, step)
-        pred_val = np.argmax(pred_val, 2)
+        pred_val = np.argmax(pred_val, axis=2)
         correct = np.sum(pred_val == current_label[start_idx:end_idx])
         total_correct += correct
         total_seen += (BATCH_SIZE*NUM_POINT)
         loss_sum += loss_val
+
+        # create new data with x,y,z,label
+        for i in range(len(pred_val[0])):
+            data.append([
+                myFloat(current_data[start_idx:end_idx, :, :][0][i][0]),
+                myFloat(current_data[start_idx:end_idx, :, :][0][i][1]),
+                myFloat(current_data[start_idx:end_idx, :, :][0][i][2]),
+                pred_val[0][i]])
+
+    if epoch % 5 == 0:
+        # use pred_val to export classification result (prediction)
+        # export to csv for now
+        outfilename = "/home/felix/Desktop/prj/data/testtest/out/test" + str(epoch) + ".csv"
+        
+        # export all batches at once
+        with open(outfilename, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(data)
     
     log_string('mean loss: %f' % (loss_sum / float(num_batches)))
     log_string('accuracy: %f' % (total_correct / float(total_seen)))
